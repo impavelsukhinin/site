e8dd9ab3018b2e06763ce6d1dcb570ca5901c200
impavelsukhinin <p.sukhinin@rocketguys.com>
2019-01-19 21:51:36 +0300

    +my website

---
+++ dev/js/main.js
@@ -0,0 +1,59 @@
+const App = () => {
+	const tags = document.querySelector('input[name="keywords"]').value.split(',')
+	const monokaiColors = [
+		'#F92672',
+		'#66D9EF',
+		'#A6E22E',
+		'#FD971F',
+		'#FFFFFF'
+	]
+	const preloader = document.querySelector('.preload')
+
+	const colorize = () => {
+		let time = 1
+		let spans = document.querySelectorAll('.tags span')
+
+		spans.forEach(el => {
+			setTimeout(function (el) {
+				addClass('active', el)
+			}, time, el)
+			time += 4
+		})
+	}
+
+	const compareRandom = () => Math.random() - 0.5
+
+	const randomInteger = (min, max) => {
+		const rand = min - 0.5 + Math.random() * (max - min + 1)
+		return Math.round(rand)
+	}
+
+	const addClass = (className, el) => {
+		if (el.classList) el.classList.add(className)
+		else el.className += ' ' + className
+	}
+
+	const height = document.body.clientHeight + 50
+	const tagsBlock = document.querySelector('.tags')
+	const newState = () => {
+		do {
+			tags.sort(compareRandom)
+			tags.forEach(function (e) {
+				tagsBlock.insertAdjacentHTML('beforeend', 
+				'<span style="color:' + monokaiColors[randomInteger(0, 4)] + '">' + e + '</span>')
+			})
+		} while (tagsBlock.offsetHeight < height)
+
+		colorize()
+	}
+
+
+	addClass('disable', preloader)
+	newState()
+
+	window.onresize = () => {
+		newState()
+	}
+}
+
+App()
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const isProd = process.env.NODE_ENV === 'production'
+const PORT = 3000
+
+const folder = isProd ? './public' : './dev'
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, folder));
+app.use(express.static(folder))
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Pavel Sukhinin | web and android developer',
+		keywords: `cocos creator,vds,linux,yandex direct,rest,html,css,xml,xhtml,javascript,
+				wordpress,bitrix,nodejs,es6,java,adaptive layout,php,android,bootstrap,website,
+				less,sass,angularjs,mongodb,mysql,jquery,grid,webpack,gulp,git,phpstorm,webstorm,
+				laravel,react,redux,🤓,😊,🤔,😎`,
+		phone: '+7 (999) 476 51 61',
+		mail: 'impavelsukhinin@yandex.ru',
+		vk: 'https://vk.com/id175204143',
+		fb: 'https://www.facebook.com/im.badjest',
+		isProd
+	})
+})
+
+app.listen(PORT)
+console.log(`Сервер запущен на ${PORT} порту в ${isProd ? 'production' : 'develop'} режиме`)
