04ec42bd6ac3fe02bd900347f1b607debecb1e08
impavelsukhinin <p.sukhinin@rocketguys.com>
2019-01-20 15:55:59 +0300

    change title

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.static(folder))
 
 app.get('/', (req, res) => {
 	res.render('index', {
-		title: 'Pavel Sukhinin | web and android developer',
-		keywords: `cocos creator,vds,linux,yandex direct,rest api,html,css,xml,xhtml,javascript,
+		title: 'Pavel Sukhinin | Web developer',
+		keywords: `cocos creator,vds,linux,yandex direct,rest api,html,css,xml,javascript,
 				wordpress,bitrix,nodejs,es6,java,adaptive layout,php,android,bootstrap,website,
 				less,sass,angularjs,mongodb,mysql,jquery,grid,webpack,gulp,git,phpstorm,webstorm,
 				laravel,react,redux,typescript,🤓,😊,🤔,😎`,
